e9a7d7e36178db036bd6d725029793660effb55d
Napoleon Guardado <napo.gv@hotmail.com>
2018-01-04 06:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,11 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
